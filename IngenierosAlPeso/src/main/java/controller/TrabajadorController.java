12ffea9eabb829ad8870728cd8206b09ed58cab5
DanielaDurand <daniela.durand@edu.uah.es>
2021-01-07 14:19:01 +0100

    TrabajadorController omitiendo la información del log.

--- IngenierosAlPeso/src/main/java/controller/TrabajadorController.java
+++ IngenierosAlPeso/src/main/java/controller/TrabajadorController.java
@@ -7,100 +7,87 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import model.Trabajador;
+import util.TrabajadorDao;
+//import util.Log;
 /**
  *
- * @author alvarikoke
+ * @author Usuario
  */
-@WebServlet(name = "TrabajadorController", urlPatterns = {"/TrabajadorController"})
-public class TrabajadorController extends HttpServlet {
+public class TrabajadorController extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "_";
+    private static String LIST_TRABAJADORES = "_";
+    private TrabajadorDao dao;
+    //private Log log;
+    
+    public TrabajadorController () {
+        super();
+        dao = new TrabajadorDao();
+    }
 
-//    /**
-//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-//     * methods.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        String forward = "";
-//        Log.log.info("Entramos en el doGet");
-//        String action = request.getParameter("action");
-//        Log.log.info("Recogemos el parametro action con valor " + action);
-//        if (action.equalsIgnoreCase("delete")) {
-//            Log.log.info("Parametro valor DELETE");
-//            int userId = Integer.parseInt(request.getParameter("userId"));
-//            dao.deleteUser(userId);
-//            forward = LIST_USER;
-//            request.setAttribute("users", dao.getAllUsers());
-//        } else if (action.equalsIgnoreCase("edit")) {
-//            Log.log.info("Parametro valor EDIT");
-//            forward = INSERT_OR_EDIT;
-//            int userId = Integer.parseInt(request.getParameter("userId"));
-//            User user = dao.getUserById(userId);
-//            request.setAttribute("user", user);
-//        } else if (action.equalsIgnoreCase("listUser")) {
-//            Log.log.info("Parametro valor LIST");
-//            forward = LIST_USER;
-//            request.setAttribute("users", dao.getAllUsers());
-//        } else {
-//            Log.log.info("Parametro valor vacio vamos a insertar");
-//            forward = INSERT_OR_EDIT;
-//        }
-//        RequestDispatcher view = request.getRequestDispatcher(forward);
-//        view.forward(request, response);
-//        return;
-//    }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        Log.log.info("Entramos por el doPost");
-///*        processRequest(request, response); */
-//        User user = new User();
-//        user.setFirstName(request.getParameter("firstName"));
-//        user.setLastName(request.getParameter("lastName"));                
-//        user.setEmail(request.getParameter("email"));
-//        String userid = request.getParameter("userid");
-//        if (userid == null || userid.isEmpty()) {
-//            Log.log.info("Vamos a añadir el usuario");
-//            dao.addUser(user);
-//        } else {
-//            user.setUserid(Integer.parseInt(userid));
-//            dao.updateUser(user);
-//        }
-//        request.setAttribute("users", dao.getAllUsers());
-//        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);            
-//        view.forward(request, response);
-//        return;
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forward = "";
+        String action = request.getParameter("action");
+        
+        if (action.equalsIgnoreCase("delete")) {
+            int id_trabajador = Integer.parseInt(request.getParameter("id_trabajador"));
+            dao.deleteTrabajador(id_trabajador);
+            forward = LIST_TRABAJADORES;
+            request.setAttribute("trabajadores", /*dao.getAllUsers()*/);
+        } else if (action.equalsIgnoreCase("edit")) {
+            forward = INSERT_OR_EDIT;
+            int id_trabajador = Integer.parseInt(request.getParameter("id_trabajador"));
+            Trabajador trabajador = dao.getTrabajadorById(id_trabajador);
+            request.setAttribute("trabajador", trabajador);
+        } else if (action.equalsIgnoreCase("listTrabajador")) {
+            forward = LIST_TRABAJADORES;
+            request.setAttribute("trabajadores", /*dao.getAllUsers()*/);
+        } else { //insert
+            forward = INSERT_OR_EDIT;
+        }
+        
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+        return;
+    } 
 
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Trabajador trabajador = new Trabajador();
+        trabajador.setNombre(request.getParameter("_"));
+        trabajador.setApellidos(request.getParameter("_")); 
+        trabajador.setUsuario(request.getParameter("_"));  
+        trabajador.setPassword(request.getParameter("_"));  
+        trabajador.setDepartamento(request.getParameter("_"));
+        String id_trabajador = request.getParameter("_");
+        if (id_trabajador == null || id_trabajador.isEmpty()) {
+            dao.addTrabajador(trabajador);
+        } else {
+            trabajador.setId_trabajador(Integer.parseInt(id_trabajador));
+            dao.updateTrabajador(trabajador);
+        }
+        request.setAttribute("trabajadores", /*dao.getAllUsers()*/);
+        RequestDispatcher view = request.getRequestDispatcher(LIST_TRABAJADORES);            
+        view.forward(request, response);
+        return;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return super.getServletInfo(); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+        
+    
 }
