546ca1926a8f97fa261d95d882ec5c9624ef4af1
JonnyC12 <jonathan.coque@edu.uah.es>
2021-01-08 13:28:48 +0100

    Actualizacion de id de las tablas

--- DDBB/IaP.sql
+++ DDBB/IaP.sql
@@ -36,11 +36,11 @@ CREATE TABLE IF NOT EXISTS `Web`.`solicitud` (
   `fecha_final` DATE NULL,
   `observacion` VARCHAR(45) NULL,
   `tramitada` TINYINT NULL,
-  `trabajador_id_trabajador` INT NOT NULL,
+  `id_trabajador` INT NOT NULL,
   PRIMARY KEY (`id_solicitud`),
-  INDEX `fk_solicitud_trabajador1_idx` (`trabajador_id_trabajador` ASC) VISIBLE,
+  INDEX `fk_solicitud_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
   CONSTRAINT `fk_solicitud_trabajador1`
-    FOREIGN KEY (`trabajador_id_trabajador`)
+    FOREIGN KEY (`id_trabajador`)
     REFERENCES `Web`.`trabajador` (`id_trabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -61,19 +61,19 @@ ENGINE = InnoDB;
 -- Table `Web`.`relacion_empresa_trabajador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Web`.`relacion_empresa_trabajador` (
-  `trabajador_id_trabajador` INT NOT NULL,
-  `empresa_id_empresa` INT NOT NULL,
+  `id_trabajador` INT NOT NULL,
+  `id_empresa` INT NOT NULL,
   `departamento` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`trabajador_id_trabajador`, `empresa_id_empresa`),
-  INDEX `fk_trabajador_has_empresa_empresa1_idx` (`empresa_id_empresa` ASC) VISIBLE,
-  INDEX `fk_trabajador_has_empresa_trabajador1_idx` (`trabajador_id_trabajador` ASC) VISIBLE,
+  PRIMARY KEY (`id_trabajador`, `id_empresa`),
+  INDEX `fk_trabajador_has_empresa_empresa1_idx` (`id_empresa` ASC) VISIBLE,
+  INDEX `fk_trabajador_has_empresa_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
   CONSTRAINT `fk_trabajador_has_empresa_trabajador1`
-    FOREIGN KEY (`trabajador_id_trabajador`)
+    FOREIGN KEY (`id_trabajador`)
     REFERENCES `Web`.`trabajador` (`id_trabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_trabajador_has_empresa_empresa1`
-    FOREIGN KEY (`empresa_id_empresa`)
+    FOREIGN KEY (`id_empresa`)
     REFERENCES `Web`.`empresa` (`id_empresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -87,11 +87,11 @@ CREATE TABLE IF NOT EXISTS `Web`.`proyecto` (
   `id_proyecto` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NULL,
   `informacion` VARCHAR(45) NULL,
-  `empresa_id_empresa` INT NOT NULL,
+  `id_empresa` INT NOT NULL,
   PRIMARY KEY (`id_proyecto`),
-  INDEX `fk_proyecto_empresa1_idx` (`empresa_id_empresa` ASC) VISIBLE,
+  INDEX `fk_proyecto_empresa1_idx` (`id_empresa` ASC) VISIBLE,
   CONSTRAINT `fk_proyecto_empresa1`
-    FOREIGN KEY (`empresa_id_empresa`)
+    FOREIGN KEY (`id_empresa`)
     REFERENCES `Web`.`empresa` (`id_empresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -102,18 +102,18 @@ ENGINE = InnoDB;
 -- Table `Web`.`relacion_proyecto_trabajador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Web`.`relacion_proyecto_trabajador` (
-  `proyecto_id_proyecto` INT NOT NULL,
-  `trabajador_id_trabajador` INT NOT NULL,
-  PRIMARY KEY (`proyecto_id_proyecto`, `trabajador_id_trabajador`),
-  INDEX `fk_proyecto_has_trabajador_trabajador1_idx` (`trabajador_id_trabajador` ASC) VISIBLE,
-  INDEX `fk_proyecto_has_trabajador_proyecto1_idx` (`proyecto_id_proyecto` ASC) VISIBLE,
+  `id_proyecto` INT NOT NULL,
+  `id_trabajador` INT NOT NULL,
+  PRIMARY KEY (`id_proyecto`, `id_trabajador`),
+  INDEX `fk_proyecto_has_trabajador_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
+  INDEX `fk_proyecto_has_trabajador_proyecto1_idx` (`id_proyecto` ASC) VISIBLE,
   CONSTRAINT `fk_proyecto_has_trabajador_proyecto1`
-    FOREIGN KEY (`proyecto_id_proyecto`)
+    FOREIGN KEY (`id_proyecto`)
     REFERENCES `Web`.`proyecto` (`id_proyecto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_proyecto_has_trabajador_trabajador1`
-    FOREIGN KEY (`trabajador_id_trabajador`)
+    FOREIGN KEY (`id_trabajador`)
     REFERENCES `Web`.`trabajador` (`id_trabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -126,17 +126,17 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Web`.`horas_jornada` (
   `horas` INT NULL,
   `fecha` DATETIME NULL,
-  `proyecto_id_proyecto` INT NOT NULL,
-  `trabajador_id_trabajador` INT NOT NULL,
-  INDEX `fk_horas_jornada_proyecto1_idx` (`proyecto_id_proyecto` ASC) VISIBLE,
-  INDEX `fk_horas_jornada_trabajador1_idx` (`trabajador_id_trabajador` ASC) VISIBLE,
+  `id_proyecto` INT NOT NULL,
+  `id_trabajador` INT NOT NULL,
+  INDEX `fk_horas_jornada_proyecto1_idx` (`id_proyecto` ASC) VISIBLE,
+  INDEX `fk_horas_jornada_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
   CONSTRAINT `fk_horas_jornada_proyecto1`
-    FOREIGN KEY (`proyecto_id_proyecto`)
+    FOREIGN KEY (`id_proyecto`)
     REFERENCES `Web`.`proyecto` (`id_proyecto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_horas_jornada_trabajador1`
-    FOREIGN KEY (`trabajador_id_trabajador`)
+    FOREIGN KEY (`id_trabajador`)
     REFERENCES `Web`.`trabajador` (`id_trabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
