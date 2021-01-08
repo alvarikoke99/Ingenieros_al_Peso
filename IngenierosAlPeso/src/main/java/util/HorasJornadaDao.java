4f4de5d31c395c45e3b9aae3b33ea43ec7263dae
Alvarikoke99 <alvarobarchin@hotmail.es>
2021-01-08 16:31:46 +0100

    Prototipo HorasJornadaDao

--- IngenierosAlPeso/src/main/java/util/HorasJornadaDao.java
+++ IngenierosAlPeso/src/main/java/util/HorasJornadaDao.java
@@ -28,46 +28,22 @@ public class HorasJornadaDao {
         connection = DbUtil.getConnection();
     }
 
-    public void addTrabajador(Horas_jornada trabajador) {
+    public void addHorasJornada(Horas_jornada horasJornada) {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into horas_jornada(nombre,apellidos, dni) values (?, ?, ? )");
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into horas_jornada(horas, fecha, id_proyecto, id_trabajador) values (?, ?, ?, ?)");
             // Parameters start with 1 
-            preparedStatement.setString(1, trabajador.getNombre());
-            preparedStatement.setString(2, trabajador.getApellidos());            
-            preparedStatement.setString(3, trabajador.getDni());
+            preparedStatement.setInt(1, horasJornada.getHoras());
+            preparedStatement.setDate(2, horasJornada.getFecha());            
+            preparedStatement.setInt(3, horasJornada.getIdProyecto());
+            preparedStatement.setInt(4, horasJornada.getIdTrabajador());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             Log.logdb.error("SQL Exception: " + e);
         }
     }
 
-    public void deleteTrabajador(int idHorasJornada) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("delete from horas_jornada where userid=?");
-            // Parameters start with 1 
-            preparedStatement.setInt(1, userId);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            Log.logdb.error("SQL Exception: " + e);
-        }
-    }
-
-    public void updateTrabajador(Horas_jornada trabajador) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("update horas_jornada set nombre=?, apellidos=?, dni=?" + "where id_trabajador=?");
-            // Parameters start with 1 
-            preparedStatement.setString(1, trabajador.getNombre());
-            preparedStatement.setString(2, trabajador.getApellidos());            
-            preparedStatement.setString(3, trabajador.getDni());
-            preparedStatement.setInt(4, trabajador.getId_trabajador());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            Log.logdb.error("SQL Exception: " + e);            
-        }
-    }
-
-    public List<Horas_jornada> getAllTrabajadores() {
-        List<Horas_jornada> dbTrabajador = new ArrayList<Horas_jornada>();
+    public List<Horas_jornada> getAllHorasJornadas() {
+        List<Horas_jornada> dbHorasJornada = new ArrayList<Horas_jornada>();
         if (connection != null)
         {
             try {
@@ -75,40 +51,109 @@ public class HorasJornadaDao {
                 ResultSet rs = statement.executeQuery("select * from horas_jornada;");
                 while (rs.next()) {
                     Horas_jornada trabajador = new Horas_jornada();
-                    trabajador.setId_trabajador(rs.getInt("userid"));
-                    trabajador.setNombre(rs.getString("firstname"));
-                    trabajador.setApellidos(rs.getString("lastname"));
-                    trabajador.setDni(rs.getString("dni"));              
-                    dbTrabajador.add(trabajador);
+                    trabajador.setHoras(rs.getInt("userid"));
+                    trabajador.setFecha(rs.getDate("fecha"));
+                    trabajador.setIdProyecto(rs.getInt("id_proyecto"));
+                    trabajador.setIdTrabajador(rs.getInt("id_trabajador"));              
+                    dbHorasJornada.add(trabajador);
                 }
             } catch (SQLException e) {
                 Log.logdb.error("SQL Exception: " + e);            
             }
-            return dbTrabajador;
+            return dbHorasJornada;
+        }
+        else
+        {
+            Log.logdb.error("No hay conexion con la bbdd");
+            return null;
+        }   
+    }
+
+    public List<Horas_jornada> getHorasJornadaByIdProyecto(int idProyecto) {
+        List<Horas_jornada> dbHorasJornada = new ArrayList<Horas_jornada>();
+        if (connection != null)
+        {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from horas_jornada where id_proyecto=?;");
+                // Parameters start with 1 
+                preparedStatement.setInt(1, idProyecto);
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next()) {
+                    Horas_jornada trabajador = new Horas_jornada();
+                    trabajador.setHoras(rs.getInt("userid"));
+                    trabajador.setFecha(rs.getDate("fecha"));
+                    trabajador.setIdProyecto(rs.getInt("id_proyecto"));
+                    trabajador.setIdTrabajador(rs.getInt("id_trabajador"));              
+                    dbHorasJornada.add(trabajador);
+                }
+            } catch (SQLException e) {
+                Log.logdb.error("SQL Exception: " + e);            
+            }
+            return dbHorasJornada;
         }
         else
         {
             Log.logdb.error("No hay conexion con la bbdd");
             return null;
         }
-       
     }
-
-    public Horas_jornada getTrabajadorById(int idHorasJornada) {
-        Horas_jornada trabajador = new Horas_jornada();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from horas_jornada where id_trabajador=?");
-            preparedStatement.setInt(1, idHorasJornada);
-            ResultSet rs = preparedStatement.executeQuery();
-            if (rs.next()) {
-                trabajador.setId_trabajador(rs.getInt("userid"));
-                trabajador.setNombre(rs.getString("firstname"));
-                trabajador.setApellidos(rs.getString("lastname"));
-                trabajador.setDni(rs.getString("dni"));
+    
+    public List<Horas_jornada> getHorasJornadaByIdTrabajador(int idTrabajador) {
+        List<Horas_jornada> dbHorasJornada = new ArrayList<Horas_jornada>();
+        if (connection != null)
+        {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from horas_jornada where id_trabajador=?;");
+                // Parameters start with 1 
+                preparedStatement.setInt(1, idTrabajador);
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next()) {
+                    Horas_jornada trabajador = new Horas_jornada();
+                    trabajador.setHoras(rs.getInt("userid"));
+                    trabajador.setFecha(rs.getDate("fecha"));
+                    trabajador.setIdProyecto(rs.getInt("id_proyecto"));
+                    trabajador.setIdTrabajador(rs.getInt("id_trabajador"));              
+                    dbHorasJornada.add(trabajador);
+                }
+            } catch (SQLException e) {
+                Log.logdb.error("SQL Exception: " + e);            
             }
-        } catch (SQLException e) {
-            Log.logdb.error("SQL Exception: " + e);
+            return dbHorasJornada;
         }
-        return trabajador;
+        else
+        {
+            Log.logdb.error("No hay conexion con la bbdd");
+            return null;
+        }
+    }
+    
+    public List<Horas_jornada> getHorasJornadaById(int idProyecto, int idTrabajador) {
+        List<Horas_jornada> dbHorasJornada = new ArrayList<Horas_jornada>();
+        if (connection != null)
+        {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from horas_jornada where id_proyecto=? and id_trabajador=?;");
+                // Parameters start with 1 
+                preparedStatement.setInt(1, idProyecto);
+                preparedStatement.setInt(2, idTrabajador);
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next()) {
+                    Horas_jornada trabajador = new Horas_jornada();
+                    trabajador.setHoras(rs.getInt("userid"));
+                    trabajador.setFecha(rs.getDate("fecha"));
+                    trabajador.setIdProyecto(rs.getInt("id_proyecto"));
+                    trabajador.setIdTrabajador(rs.getInt("id_trabajador"));              
+                    dbHorasJornada.add(trabajador);
+                }
+            } catch (SQLException e) {
+                Log.logdb.error("SQL Exception: " + e);            
+            }
+            return dbHorasJornada;
+        }
+        else
+        {
+            Log.logdb.error("No hay conexion con la bbdd");
+            return null;
+        }
     }
 }
