93dae4c6d08f40d905b968ceed887debbb498c7c
JonnyC12 <jonathan.coque@edu.uah.es>
2021-01-25 13:01:01 +0100

    Nueva base de datos

--- DDBB/IaP.sql
+++ DDBB/IaP.sql
@@ -5,158 +5,183 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Web
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema web
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema Web
+-- Schema web
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Web` ;
-USE `Web` ;
+CREATE SCHEMA IF NOT EXISTS `web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `web` ;
 
 -- -----------------------------------------------------
--- Table `Web`.`trabajador`
+-- Table `web`.`empresa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`trabajador` (
-  `id_trabajador` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `apellidos` VARCHAR(45) NULL,
-  `dni` VARCHAR(45) NULL,
-  `ultima_jornada` DATETIME NULL,
-  PRIMARY KEY (`id_trabajador`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Web`.`solicitud`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`solicitud` (
-  `id_solicitud` INT NOT NULL AUTO_INCREMENT,
-  `tipo` VARCHAR(45) NULL,
-  `fecha_ini` DATE NULL,
-  `fecha_final` DATE NULL,
-  `observacion` VARCHAR(45) NULL,
-  `tramitada` TINYINT NULL,
-  `id_trabajador` INT NOT NULL,
-  PRIMARY KEY (`id_solicitud`),
-  CONSTRAINT `fk_solicitud_trabajador1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`trabajador` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Web`.`empresa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`empresa` (
+CREATE TABLE IF NOT EXISTS `web`.`empresa` (
   `id_empresa` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `descripcion` VARCHAR(45) NULL,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `descripcion` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id_empresa`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `Web`.`relacion_empresa_trabajador`
+-- Table `web`.`proyecto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`relacion_empresa_trabajador` (
-  `id_trabajador` INT NOT NULL,
-  `id_empresa` INT NOT NULL,
-  `departamento` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_trabajador`, `id_empresa`),
-  CONSTRAINT `fk_trabajador_has_empresa_trabajador1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`trabajador` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_trabajador_has_empresa_empresa1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`empresa` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Web`.`proyecto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`proyecto` (
+CREATE TABLE IF NOT EXISTS `web`.`proyecto` (
   `id_proyecto` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `informacion` VARCHAR(45) NULL,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `informacion` VARCHAR(45) NULL DEFAULT NULL,
   `id_empresa` INT NOT NULL,
   PRIMARY KEY (`id_proyecto`),
   INDEX `fk_proyecto_empresa1_idx` (`id_empresa` ASC) VISIBLE,
   CONSTRAINT `fk_proyecto_empresa1`
     FOREIGN KEY (`id_empresa`)
-    REFERENCES `Web`.`empresa` (`id_empresa`)
+    REFERENCES `web`.`empresa` (`id_empresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `Web`.`relacion_proyecto_trabajador`
+-- Table `web`.`trabajador`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`relacion_proyecto_trabajador` (
+CREATE TABLE IF NOT EXISTS `web`.`trabajador` (
+  `id_trabajador` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `apellidos` VARCHAR(45) NULL DEFAULT NULL,
+  `dni` VARCHAR(45) NULL DEFAULT NULL,
+  `ultima_jornada` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id_trabajador`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `web`.`horas_jornada`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `web`.`horas_jornada` (
+  `horas` INT NULL DEFAULT NULL,
+  `fecha` DATE NULL DEFAULT NULL,
   `id_proyecto` INT NOT NULL,
   `id_trabajador` INT NOT NULL,
-  PRIMARY KEY (`id_proyecto`, `id_trabajador`),
-  CONSTRAINT `fk_proyecto_has_trabajador_proyecto1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`proyecto` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_proyecto_has_trabajador_trabajador1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`trabajador` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Web`.`horas_jornada`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`horas_jornada` (
-  `horas` INT NULL,
-  `fecha` DATE NULL,
-  `id_proyecto` INT NOT NULL,
-  `id_trabajador` INT NOT NULL,
+  INDEX `fk_horas_jornada_proyecto1_idx` (`id_proyecto` ASC) VISIBLE,
+  INDEX `fk_horas_jornada_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
   CONSTRAINT `fk_horas_jornada_proyecto1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`proyecto` ()
+    FOREIGN KEY (`id_proyecto`)
+    REFERENCES `web`.`proyecto` (`id_proyecto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_horas_jornada_trabajador1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`trabajador` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`id_trabajador`)
+    REFERENCES `web`.`trabajador` (`id_trabajador`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `Web`.`registro_jornada`
+-- Table `web`.`registro_jornada`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Web`.`registro_jornada` (
+CREATE TABLE IF NOT EXISTS `web`.`registro_jornada` (
   `fecha_entrada` DATETIME NOT NULL,
   `fecha_salida` DATETIME NOT NULL,
   `id_trabajador` INT NOT NULL,
   `id_proyecto` INT NOT NULL,
-  CONSTRAINT `fk_registro_jornada_trabajador1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`trabajador` ()
+  INDEX `fk_registro_jornada_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
+  INDEX `fk_registro_jornada_proyecto1_idx` (`id_proyecto` ASC) VISIBLE,
+  CONSTRAINT `fk_registro_jornada_proyecto1`
+    FOREIGN KEY (`id_proyecto`)
+    REFERENCES `web`.`proyecto` (`id_proyecto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_registro_jornada_proyecto1`
-    FOREIGN KEY ()
-    REFERENCES `Web`.`proyecto` ()
+  CONSTRAINT `fk_registro_jornada_trabajador1`
+    FOREIGN KEY (`id_trabajador`)
+    REFERENCES `web`.`trabajador` (`id_trabajador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `web`.`relacion_empresa_trabajador`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `web`.`relacion_empresa_trabajador` (
+  `id_trabajador` INT NOT NULL,
+  `id_empresa` INT NOT NULL,
+  `departamento` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_trabajador`, `id_empresa`),
+  INDEX `fk_trabajador_has_empresa_empresa1_idx` (`id_empresa` ASC) VISIBLE,
+  INDEX `fk_trabajador_has_empresa_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
+  CONSTRAINT `fk_trabajador_has_empresa_empresa1`
+    FOREIGN KEY (`id_empresa`)
+    REFERENCES `web`.`empresa` (`id_empresa`)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_trabajador_has_empresa_trabajador1`
+    FOREIGN KEY (`id_trabajador`)
+    REFERENCES `web`.`trabajador` (`id_trabajador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `web`.`relacion_proyecto_trabajador`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `web`.`relacion_proyecto_trabajador` (
+  `id_proyecto` INT NOT NULL,
+  `id_trabajador` INT NOT NULL,
+  PRIMARY KEY (`id_proyecto`, `id_trabajador`),
+  INDEX `fk_proyecto_has_trabajador_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
+  INDEX `fk_proyecto_has_trabajador_proyecto1_idx` (`id_proyecto` ASC) VISIBLE,
+  CONSTRAINT `fk_proyecto_has_trabajador_proyecto1`
+    FOREIGN KEY (`id_proyecto`)
+    REFERENCES `web`.`proyecto` (`id_proyecto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_proyecto_has_trabajador_trabajador1`
+    FOREIGN KEY (`id_trabajador`)
+    REFERENCES `web`.`trabajador` (`id_trabajador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `web`.`solicitud`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `web`.`solicitud` (
+  `id_solicitud` INT NOT NULL AUTO_INCREMENT,
+  `tipo` VARCHAR(45) NULL DEFAULT NULL,
+  `fecha_ini` DATE NULL DEFAULT NULL,
+  `fecha_final` DATE NULL DEFAULT NULL,
+  `observacion` VARCHAR(45) NULL DEFAULT NULL,
+  `tramitada` TINYINT NULL DEFAULT NULL,
+  `id_trabajador` INT NOT NULL,
+  PRIMARY KEY (`id_solicitud`),
+  INDEX `fk_solicitud_trabajador1_idx` (`id_trabajador` ASC) VISIBLE,
+  CONSTRAINT `fk_solicitud_trabajador1`
+    FOREIGN KEY (`id_trabajador`)
+    REFERENCES `web`.`trabajador` (`id_trabajador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
